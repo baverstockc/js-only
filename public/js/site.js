a438c34d0db0658dc2c492bdf9b3429919bdcb5c
Christian Baverstock <baverstockchristian@gmail.com>
2015-07-31 13:50:30 +0100

    Update forms

--- public/js/site.js
+++ public/js/site.js
@@ -1,4 +1,4 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
+var app = angular.module('PatientApp', ['ngMaterial']);
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
@@ -6,3 +6,24 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   };
  
 }]);
+
+app.controller('DemoCtrl', function($scope) {
+    $scope.user = {
+      title: 'Mr',
+      email: 'ipsum@lorem.com',
+      firstName: 'Christian',
+      lastName: 'Baverstock' ,
+      company: 'Google' ,
+      address: '59 otter Close' ,
+      city: 'Stratford' ,
+      county: 'London' ,
+      summary: 'Stuff',
+      postalCode : 'E15 2PZ'
+    };
+  })
+  .config( function($mdThemingProvider){
+    // Configure a dark theme with primary foreground yellow
+    $mdThemingProvider.theme('docs-dark', 'default')
+        .primaryPalette('yellow')
+        .dark();
+  });
